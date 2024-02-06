0eff6c1244111c4add5f2cfb2854ea50711f186d
xaukael <37848032+xaukael@users.noreply.github.com>
2024-02-06 07:48:43 -0600

    Add files via upload

--- canvas-fade-in-out.js
+++ canvas-fade-in-out.js
@@ -11,11 +11,11 @@ var fadeCanvas = function() {
 Hooks.on('canvasInit', (canvas)=>{
   if (game.user.isGM && !game.settings.get('canvas-fade-in-out', 'forGM')) return;
   if (game.settings.get('canvas-fade-in-out', 'visible')) return;
-  $('body').append(`<style id="canvas-fade-style"></style>`)
+  if (! $('#canvas-fade-style').length) $('body').append(`<style id="canvas-fade-style"></style>`)
   let background = game.settings.get('canvas-fade-in-out', 'background')
   if (background.includes(';')) $('#canvas-fade-style').text(`body{${background}}`)
   else $('#canvas-fade-style').text(`body{background:${background};}`)
-  $('body').css({background:game.settings.get('canvas-fade-in-out', 'background')})
+  //$('body').css({background:game.settings.get('canvas-fade-in-out', 'background')})
   $("#board").hide();
 });
 
